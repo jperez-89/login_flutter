c4368d659cc1b2117bfd077db19322080a6a27c4
Jairo <jairo.perez@centaurosolutions.com>
2023-10-10 13:34:04 -0600

    Actualizar vista - Bottom NavigationBar

--- lib/screens/dashboard_pega_screen.dart
+++ lib/screens/dashboard_pega_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:login_flutter/models/actions/case_actions.dart';
+import 'package:login_flutter/theme/app_theme.dart';
 import 'package:login_flutter/widgets/drawer.dart';
 import 'package:login_flutter/widgets/worklist.dart';
 
@@ -40,18 +41,32 @@ class _DashboardPegaScreenState extends State<DashboardPegaScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: AppTheme.primaryColor,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+          ]),
       drawer: DrawerWidget(lst: lst, service: service),
-      appBar: AppBar(title: const Text('Dashboard'), actions: [
-        Container(
-            margin: const EdgeInsets.only(right: 10),
-            child: ElevatedButton(
-                onPressed: () {
-                  // getCaseType();
-                },
-                child: const CircleAvatar(
-                  child: Text('I'),
-                )))
-      ]),
+      appBar: AppBar(
+          backgroundColor: AppTheme.primaryColor,
+          title: const Text('Dashboard'),
+          actions: [
+            Container(
+                // color: AppTheme.primaryColor,
+                margin: const EdgeInsets.only(right: 10),
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll(AppTheme.primaryColor)),
+                    onPressed: () {
+                      // getCaseType();
+                    },
+                    child: const CircleAvatar(
+                      backgroundColor: AppTheme.secondaryColor,
+                      child: Text('I'),
+                    )))
+          ]),
       body: const WorklistWidget(),
     );
   }
